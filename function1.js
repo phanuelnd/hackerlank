7e28bcedf452cad064a6fbd4d7213525420674d9
InnocentThecent <muragijimanainnocent3@gmail.com>
2023-03-15 01:24:39 +0200

    Hey there just for test

--- function1.js
+++ function1.js
@@ -3,10 +3,12 @@ function sum(arr)
     let sum =0;
     for(let i =0; i<arr.length; i++)
     {
+        if(i%2 != 0){
         sum=sum+arr[i];
+        }
 
     }
     return sum;
     
 }
-console.log(sum([10,30,10]));
+console.log(sum([33,17,10]));
