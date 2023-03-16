06f49b2996295f0bbd767ffe4bcb4fff55ea49cc
ndikumanaphanuel@gmail.com <muragijimanainnocent3@gmail.com>
2023-03-16 02:14:56 +0200

    Returning double variables

--- function1.js
+++ function1.js
@@ -1,14 +1,35 @@
-function sum(arr)
+// function sum(arr)
+// {
+//     let sum =0;
+//     for(let i =0; i<arr.length; i++)
+//     {
+//         if(i%2 != 0){
+//         sum=sum+arr[i];
+//         }
+
+//     }
+//     return sum;
+    
+// }
+// console.log(sum([33,17,10,22]));
+
+
+function smallest(arr)
 {
-    let sum =0;
-    for(let i =0; i<arr.length; i++)
-    {
-        if(i%2 != 0){
-        sum=sum+arr[i];
+    let min = arr[0];
+    let max = arr[0];
+    for(let i =0;i<arr.length;i++){
+        if(arr[i]<min)
+        {
+           min = arr[i];
+        }
+        if(arr[i]>max)
+        {
+            max = arr[i];
         }
 
     }
-    return sum;
-    
+    return min;
 }
-console.log(sum([33,17,10,22]));
+
+console.log(smallest([0,10,20,30,40]));
